328ba1af9de03558a01e1accb109315d46e6713a
KUBA <kuba.piecuch.kp@gmail.com>
2023-01-11 20:36:45 +0100

    added controller Template abstract class - moved some methods.
    implemented method for reading Strings and ints in different ways and showing massage in gui and console

---
+++ src/main/java/movies/controller/Controller.java
@@ -0,0 +1,96 @@
+package movies.controller;
+
+import movies.model.Movie;
+import movies.service.MovieService;
+
+import javax.swing.*;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class Controller {
+
+    private boolean running = true;
+    private MovieService movieService= new MovieService();
+    private static final String OPTIONS = """
+            Wybierz jedną z opcji:
+            1. Dodaj nowy film
+            2. Wyświetl filmy
+            3. Koniec""";
+
+    public void startMenu() {
+        do {
+            menuAction();
+        } while (running);
+    }
+
+    private void menuAction() {
+        int input = readInt(OPTIONS);
+        handleOption(input);
+    }
+
+    private void handleOption(int input) {
+        try {
+            executeOption(input);
+        } catch (SQLException e) {
+            showMessage("Błąd zapytania do BD");
+        }
+    }
+
+    private void executeOption(int input) throws SQLException{ //todo dekompozycja!
+        switch (input) {
+            case 1:
+                addMovie();
+                break;
+            case 2:
+                showMovies();
+                break;
+            case 3:
+                end();
+                break;
+        }
+    }
+
+    private void addMovie() throws SQLException {
+        Movie movie = readMovieData();
+        movieService.save(movie);
+    }
+
+    private void showMovies() throws SQLException {
+        List<Movie> movies = movieService.findAllMovies();
+        showMovies(movies);
+
+    }
+
+
+    private Movie readMovieData() {
+        String title = readString("Podaj tytuł filmu: ");
+        int premiereYear = readInt("Podaj rok premiery: ");
+        if (premiereYear < 1800 || premiereYear > 2100) {
+            showMessage("Podano nierealną datę premiery.\n" +
+                    "Powinien być przedział: 1800 - 2100");
+            return readMovieData();
+        }
+        String genre = readString("Podaj gatunek: ");
+        int rate = readInt(JOptionPane.showInputDialog("Ocena: "));
+        return new Movie(title, premiereYear, genre, rate);
+    }
+
+    private void showMovies(List<Movie> movies) {
+        String allFilms = "";
+        for (Movie movie : movies) {
+            allFilms += movie + "\n";
+        }
+        showMessage(allFilms);
+    }
+
+    private void end() throws SQLException {
+        System.out.println("Koniec");
+        running = false;
+        movieService.closeAllResources();
+    }
+
+    abstract void showMessage(String message);
+    abstract int readInt(String message);
+    abstract String readString(String message);
+
+}
--- src/main/java/movies/controller/WindowController.java
+++ src/main/java/movies/controller/WindowController.java
@@ -7,97 +7,21 @@ import javax.swing.*;
 import java.sql.SQLException;
 import java.util.List;
 
-public class WindowController {
-    private boolean running = true;
-    private String title;
-    private int premiereYear;
-    private String genre;
-    private int rate;
-    private int option;
-    private MovieService movieService= new MovieService();
+public class WindowController extends Controller {
 
-    public WindowController(){
-
+    public void showMessage(String message){
+        JOptionPane.showMessageDialog(null, message);
     }
-
-    public void startMenu() {
-        do {
-            menuAction();
-        } while (running);
-    }
-
-    private void menuAction() {
-        handleOption(getOptionFromAvailable());
-    }
-
-
-
-    private int getOptionFromAvailable() {
-        return option = Integer.parseInt(JOptionPane.showInputDialog("""
-                Wybierz jedną z opcji:
-                1. Dodaj nowy film
-                2. Wyświetl filmy
-                3. Koniec"""));
-    }
-
-    private void handleOption(int input) {
-        try {
-            executeOption(input);
-        } catch (SQLException e) {
-            System.out.println("Błąd zapytania do BD");
+    public int readInt(String message){
+        try{
+            String input = readString(message);
+            return Integer.parseInt(message);
+        }catch(NumberFormatException e){
+            showMessage("Należy podać liczbę");
+            return readInt(message);
         }
     }
-
-    private void executeOption(int input) throws SQLException{ //todo dekompozycja!
-        switch (input) {
-            case 1:
-                addMovie();
-                break;
-            case 2:
-                showMovies();
-                break;
-            case 3:
-                end();
-                break;
-        }
-    }
-
-    private void addMovie() throws SQLException {
-        Movie movie = readMovieData();
-        movieService.save(movie);
-    }
-
-    private void showMovies() throws SQLException {
-        List<Movie> movies = movieService.findAllMovies();
-        showMovies(movies);
-
-    }
-
-
-    private Movie readMovieData() {
-        this.title = JOptionPane.showInputDialog("Podaj tytuł filmu: ");
-        this.premiereYear = Integer.parseInt(JOptionPane.showInputDialog("Podaj rok premiery: "));
-        if (premiereYear < 1800 || premiereYear > 2100) {
-            JOptionPane.showMessageDialog(null, "Podano nierealną datę premiery.\n" +
-                    "Powinien być przedział: 1800 - 2100");
-            return readMovieData();
-        }
-        this.genre = JOptionPane.showInputDialog("Podaj gatunek: ");
-        this.rate = Integer.parseInt(JOptionPane.showInputDialog("Ocena: "));
-        return new Movie(title, premiereYear, genre, rate);
-    }
-
-    private void showMovies(List<Movie> movies) {
-        String allFilms = "";
-        for (Movie movie : movies) {
-            allFilms += movie + "\n";
-        }
-        JOptionPane.showMessageDialog(null, allFilms);
-    }
-
-    private void end() throws SQLException {
-        System.out.println("Koniec");
-        running = false;
-        movieService.closeAllResources();
+    public String readString(String message){
+        return JOptionPane.showInputDialog(message);
     }
 }
